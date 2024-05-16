e69f4de0b5bb63f98cce86ff8ecc5040475f2504
Yasser Baronadao <ybaronadao@kpmg.com>
2024-05-16 22:45:32 +0200

    saved dataset into json and create constants file

---
+++ utils/constants.py
@@ -0,0 +1,4 @@
+UNITS = ["zéro", "un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf", 
+             "dix", "onze", "douze", "treize", "quatorze", "quinze", "seize"]
+TENS = ["", "dix", "vingt", "trente", "quarante", "cinquante", "soixante", "septante", "huitante", "nonante"]
+FRENCH_FRANCE_TENS= ["", "dix", "vingt", "trente", "quarante", "cinquante", "soixante", "soixante-dix", "quatre-vingts", "quatre-vingt-dix"]
